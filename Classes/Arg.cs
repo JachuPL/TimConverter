235a9e9ef7d97d2efe03be0b859f85d09696dbc3
JachuPL <bonkowskijan@gmail.com>
2015-01-09 22:44:55 +0100

    Initial commit

---
+++ Classes/Arg.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimConverter
+{
+    internal class Arg
+    {
+        #region Singleton Management
+        private static Arg _instance = null;
+        private static object chekLock = new object();
+
+        private Arg() { }
+        public static Arg Instance
+        {
+            get
+            {
+                lock (chekLock)
+                {
+                    if (_instance == null)
+                        _instance = new Arg();
+                    return _instance;
+                }
+            }
+        }
+        #endregion
+
+        public void ProcessArgument(string arg)
+        {
+            if (arg[0] == '-')
+            {
+                ProcessOption(arg.Substring(1, arg.Length-1));
+            }
+        }
+
+        void ProcessOption(string arg)
+        {
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = string.Format("Tim Converter by JachuPL v{0}", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version);
+
+            /// Argument parsing \\\
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
